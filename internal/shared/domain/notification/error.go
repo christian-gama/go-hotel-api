d66ba283f561c37059f2c34819864bd17ea27922
Christian <christiangama.dev@gmail.com>
2022-07-29 10:01:48 -0300

    docs: add commentary to notification methods

--- internal/shared/domain/notification/error.go
+++ internal/shared/domain/notification/error.go
@@ -5,6 +5,9 @@ import (
 	"strings"
 )
 
+// Error returns the error message of the notification. It will concatenate all the error messages
+// with a comma and add the context to the message. For example, if the notification has two errors,
+// the message will be `context:message1,context:message2`.
 func Error(errors []*ErrorProps) error {
 	var message string
 	for _, e := range errors {
--- internal/shared/domain/notification/notification.go
+++ internal/shared/domain/notification/notification.go
@@ -5,16 +5,23 @@ import (
 	"strings"
 )
 
+// ErrorProps represents the error properties of a notification.
 type ErrorProps struct {
+	// Message is the error message.
 	Message string
+
+	// Context is the context of the error, e.g. "user".
 	Context string
 }
 
+// Notification represents the notification of a domain, which is a collection of errors.
 type Notification struct {
 	context string
 	errors  []*ErrorProps
 }
 
+// AddErrorf adds an error to the notification and formats the message with the given arguments.
+// Is equivalent to call AddError with the result of fmt.Sprintf.
 func (n *Notification) AddErrorf(message string, v ...any) {
 	n.errors = append(
 		n.errors,
@@ -22,14 +29,17 @@ func (n *Notification) AddErrorf(message string, v ...any) {
 	)
 }
 
+// HasErrors returns true if the notification has errors.
 func (n *Notification) HasErrors() bool {
 	return len(n.errors) > 0
 }
 
+// Errors returns a slice of the errors of the notification.
 func (n *Notification) Errors() []*ErrorProps {
 	return n.errors
 }
 
+// New creates a new notification with the given context.
 func New(context string) *Notification {
 	fmtContext := strings.ToLower(context[:1]) + context[1:]
 
