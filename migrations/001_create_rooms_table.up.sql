d330aea7443e6540662ce4a1db988d6b630831a4
Christian Gama <christiangama.dev@gmail.com>
2022-08-04 23:33:19 -0300

    feat: add rooms migration

---
+++ migrations/001_create_rooms_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE rooms (
+  id serial PRIMARY KEY,
+  uuid uuid NOT NULL,
+  name varchar(255) NOT NULL,
+  description text NOT NULL,
+  price decimal(10,2) NOT NULL,
+  bed_count integer NOT NULL,
+  is_available boolean NOT NULL
+);
