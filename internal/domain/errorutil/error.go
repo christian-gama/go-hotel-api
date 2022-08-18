f1a1ee7160af04169e9028a5475a003c737bf4a4
Christian Gama <christiangama.dev@gmail.com>
2022-08-18 11:55:57 -0300

    feat: add helper functions to errorutil

--- internal/domain/errorutil/error.go
+++ internal/domain/errorutil/error.go
@@ -14,3 +14,18 @@ type Error struct {
 	// Param is the name of the parameter that caused the error.
 	Param string
 }
+
+// New creates a new instance of Error.
+func New(code ErrorCode, message, param, context string) *Error {
+	return &Error{
+		Code:    code,
+		Message: message,
+		Param:   param,
+		Context: context,
+	}
+}
+
+// Append appends multiple errors into an array.
+func Append(err ...*Error) []*Error {
+	return err
+}
